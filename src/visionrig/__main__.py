89463b156e87a0abbd7093d331346e7e7f6ba9e4
Ternedal <a.nielbaek@gmail.com>
2026-09-25 20:46:15 +0200

    Wire ModelRig bridge into VisionRig service

--- src/visionrig/__main__.py
+++ src/visionrig/__main__.py
@@ -9,9 +9,11 @@ from .service_config import ServiceConfig
 def main() -> None:
     config = ServiceConfig.from_env()
     bundle = config.build_bundle()
+    modelrig_publisher = config.build_modelrig_publisher()
     app = create_app(
         bundle.pipeline,
         max_sensor_frame_bytes=config.max_sensor_frame_bytes,
+        modelrig_publisher=modelrig_publisher,
     )
     uvicorn.run(app, host="127.0.0.1", port=8110, reload=False)
 
