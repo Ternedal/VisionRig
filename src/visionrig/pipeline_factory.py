1a11e79144d5fbc3ff78ada98a66b87d746574fb
Ternedal <a.nielbaek@gmail.com>
2026-09-25 19:57:33 +0200

    Import Kinect depth stage

--- src/visionrig/pipeline_factory.py
+++ src/visionrig/pipeline_factory.py
@@ -11,7 +11,7 @@ from .embeddings import (
     EntityEmbeddingStage,
     OnnxImageEmbeddingEncoder,
 )
-from .landmarks_mediapipe import MediaPipeLandmarkStage
+from .landmarks_mediapipe import MediaPipeLandmarkStage\nfrom .kinect_v2 import KinectV2DepthStage
 from .model_manifest import (
     DepthModelManifest,
     EmbeddingModelManifest,
